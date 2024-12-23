c66c9d43614373c2a9f19416f51cca5666de68e6
nazlitirek <tireknazlihilal@gmail.com>
2024-12-24 02:09:26 +0300

    first

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace marketProjesi.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
